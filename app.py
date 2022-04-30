36cf80a61668007245ae8541a2f9dce632a4fb2a
Adilius <leugue-gamer@hotmail.com>
2022-05-01 01:23:52 +0200

    clustering done

--- app.py
+++ app.py
@@ -5,36 +5,43 @@ import app.post_proccess as post_process
 import app.util as util
 import cv2
 
-# Input image
-#input_image = cv2.imread("form_filled\\top_form\\1.png", cv2.IMREAD_COLOR)
-input_image = cv2.imread("form_blank\\top_form\\blank.png", cv2.IMREAD_COLOR)
-#util.show_image(input_image)
+
+def blank_clustering():
+    # Input image
+    #input_image = cv2.imread("form_filled\\top_form\\1.png", cv2.IMREAD_COLOR)
+    input_image = cv2.imread("form_blank\\top_form\\blank.png", cv2.IMREAD_COLOR)
+    #util.show_image(input_image)
 
 
-# ______ Preprocess ____________    
-preprocessed_image = preprocessing.preprocess_image(
-    image=input_image, grayscale=True, noise_removal=True, deskew=False
-)
-#util.show_image(preprocessed_image)
-# ______________________________
+    # ______ Preprocess ____________    
+    preprocessed_image = preprocessing.preprocess_image(
+        image=input_image, grayscale=True, noise_removal=True, deskew=False
+    )
+    #util.show_image(preprocessed_image)
+    # ______________________________
 
 
-# ______ Text detection ________
-# Create 3-layer image to fit text detection
+    # ______ Text detection ________
+    # Create 3-layer image to fit text detection
 
-merged_image = cv2.merge([preprocessed_image, preprocessed_image, preprocessed_image])
+    merged_image = cv2.merge([preprocessed_image, preprocessed_image, preprocessed_image])
 
-text_detected_image, boxes = text_detection.detect_text(
-    merged_image, threshold=0.1, overlap_threshold=0.01
-)
-# util.show_image(image_boxes)
-# ______________________________
+    text_detected_image, boxes = text_detection.detect_text(
+        merged_image, threshold=0.1, overlap_threshold=0.01
+    )
+    # util.show_image(image_boxes)
+    # ______________________________
 
 
-# ______  Text recognition ______
-boxes = text_recognition.recognize_text(preprocessed_image, boxes)
-# _______________________________
+    # ______  Text recognition ______
+    boxes = text_recognition.recognize_text(preprocessed_image, boxes)
+    # _______________________________
 
-# ________ Post-process ________
-post_process.post_process(boxes)
-# _______________________________
+
+    # ________ Post-process ________
+    post_process.post_process_blank(boxes)
+    # _______________________________
+
+if __name__ == "__main__":
+    blank_clustering()
+    pass
--- app/post_proccess.py
+++ app/post_proccess.py
@@ -2,81 +2,100 @@ import math
 import json
 import pprint
 
+# Factor to beat the average distance to be in cluster
+DISTANCE_CONSTANT = 0.9
 
 def group_fields(boxes: dict):
+
+    print(f'Clustering boxes...')
 
     #pprint.pprint(boxes)
 
     total_distance = 0
-    avg_distance = 0
 
-    for id, box in boxes.items():
+    # CALCULATING DISTANCES
+    for origin_id, origin_box in boxes.items():
         shortest_path = list()
-        pprint.pprint(box)
 
-        box_pos = (int(box["positions"]["midpoint"]["x"]), int(box["positions"]["midpoint"]["y"]))
-        print(box_pos)
+        # Get origin box position
+        origin_box_pos = (int(origin_box["positions"]["midpoint"]["x"]), int(
+            origin_box["positions"]["midpoint"]["y"]))
 
-    # For each box
-    for index, box in enumerate(boxes):
+        # iterate over all neighbors
+        for neighbor_id, neighbor_box in boxes.items():
 
-        box_pos = (box[4], box[5])
+            # Don't iterate over origin box
+            if neighbor_id != origin_id:
 
-        # For each neighbor
-        for neighbor in [x for i, x in enumerate(boxes) if i != index]:
+                # Get neighbor boxes position
+                neighbor_box_pos = (int(neighbor_box["positions"]["midpoint"]["x"]), int(
+                    neighbor_box["positions"]["midpoint"]["y"]))
 
-            neighbor_pos = (neighbor[4], neighbor[5])
+                # Calculate distance between origin and neighbor
+                weighted_eucledian_distance = math.sqrt(
+                    pow((origin_box_pos[0] - neighbor_box_pos[0])/2, 2) + pow((origin_box_pos[1] - neighbor_box_pos[1]), 2))
 
-            weighted_eucledian_distance = math.sqrt(
-                pow((box_pos[0] - neighbor_pos[0])/2, 2) + pow((box_pos[1] - neighbor_pos[1]), 2))
-
-            shortest_path.append(weighted_eucledian_distance)
-
-            print(
-                f'Index: {index}  Box: {box[-1]} {box_pos}  Neighbor: {neighbor[-1]} {neighbor_pos}  Distance: {weighted_eucledian_distance}')
+                shortest_path.append(weighted_eucledian_distance)
 
         shortest_path.sort()
         shortest_path = shortest_path[:1]
         total_distance += shortest_path[0]
-        
-    
+
+    # GET AVERAGE DISTANCE
     avg_distance = total_distance/len(boxes)
-    print(f'{avg_distance=}')
+    print(f'Average cluster distance : {avg_distance}')
 
-    # For each box
-    for index, box in enumerate(boxes):
+    # ASSIGN CLUSTER
+    for origin_id, origin_box in boxes.items():
+        
+        # Get origin box position
+        origin_box_pos = (int(origin_box["positions"]["midpoint"]["x"]), int(
+            origin_box["positions"]["midpoint"]["y"]))
 
-        box_pos = (box[4], box[5])
+        # Iterate over all neighbors
+        for neighbor_id, neighbor_box in boxes.items():
 
-        shortest_path = list()
+            # Don't iterate over origin box
+            if neighbor_id != origin_id:
 
-        # For each neighbor
-        for neighbor in [x for i, x in enumerate(boxes) if i != index]:
+                # get neighbor pos
+                neighbor_box_pos = (int(neighbor_box["positions"]["midpoint"]["x"]), int(
+                    neighbor_box["positions"]["midpoint"]["y"]))
 
-            neighbor_pos = (neighbor[4], neighbor[5])
+                # Calculate distance between origin and neighbor
+                weighted_eucledian_distance = math.sqrt(
+                    pow((origin_box_pos[0] - neighbor_box_pos[0])/2, 2) + pow((origin_box_pos[1] - neighbor_box_pos[1]), 2))
 
-            weighted_eucledian_distance = math.sqrt(
-                pow((box_pos[0] - neighbor_pos[0])/2, 2) + pow((box_pos[1] - neighbor_pos[1]), 2))
+                # Assign neighbor new cluster ID
+                if weighted_eucledian_distance <= avg_distance*0.9:
+                    boxes[str(neighbor_id)]["cluster"] = boxes[str(origin_id)]["cluster"]
 
-            if weighted_eucledian_distance <= avg_distance:
-                print(
-                    f'Index: {index}  Box: {box[-1]} {box_pos}  Neighbor: {neighbor[-1]} {neighbor_pos}  Distance: {weighted_eucledian_distance}')
+    clustered_boxes = list()
+    for _, _ in boxes.items():
+        clustered_boxes.append('')
+    for id, box in boxes.items():
+        clustered_boxes[box['cluster']] = (clustered_boxes[box['cluster']] + ' ' + box['text']).lstrip()
+    clustered_boxes = [x for x in clustered_boxes if x != '']
 
+    print(clustered_boxes)
+    pprint.pprint(boxes)
+    return boxes
 
 def fuzzy_compare(blank_words: list, recognized_words: list):
     pass
 
 # To help easier manipulate boxes
+
+
 def convert_to_dict(boxes: list):
     boxes_dict = dict()
-    
+
     for index, box in enumerate(boxes):
         boxes_dict[str(index)] = {}
         boxes_dict[str(index)]["positions"] = dict()
         boxes_dict[str(index)]["positions"]["top_left"] = dict()
         boxes_dict[str(index)]["positions"]["top_left"]["x"] = box[0]
         boxes_dict[str(index)]["positions"]["top_left"]["y"] = box[1]
-
 
         boxes_dict[str(index)]["positions"]["bottom_right"] = dict()
         boxes_dict[str(index)]["positions"]["bottom_right"]["x"] = box[2]
@@ -88,17 +107,15 @@ def convert_to_dict(boxes: list):
 
         boxes_dict[str(index)]["frame"] = True
         boxes_dict[str(index)]["text"] = box[-1]
-        boxes_dict[str(index)]["cluster"] = -1
-        
+        boxes_dict[str(index)]["cluster"] = index
+
     #pprint.pprint(boxes_dict, sort_dicts=False)
     return boxes_dict
-    
 
-def post_process(boxes: list):
 
-    
+def post_process_blank(boxes: list):
 
-    print('___ POST-PROCESS ___')
+    print('___ POST-PROCESS BLANK ___')
     boxes_dict = convert_to_dict(boxes)
 
-    group_fields(boxes_dict)
+    grouped_boxes = group_fields(boxes_dict)
